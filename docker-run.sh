2b70bbeac878048a48d985a5b332181cad6a6f0a
theSoenke <theSoenke@users.noreply.github.com>
2017-10-18 15:12:31 +0200

    Only delete nodcore content on success

--- docker-run.sh
+++ docker-run.sh
@@ -2,5 +2,4 @@
 set -e -o pipefail
 
 rm -rf corpus
-java -jar /app/NoDCore-assembly-1.0.jar --dir /app/content --format compressed
-rm -rf /app/content/*
+java -jar /app/NoDCore-assembly-1.0.jar --dir /app/content --format compressed && rm -rf /app/content/*
